c7a67bce2b831c5b532b873c707862f76ba3ed89
Tony ElHabr <anthonyelhabr@gmail.com>
2023-09-25 07:55:04 -0500

    more convert to json tinkering

--- R/x-convert-to-json.R
+++ R/x-convert-to-json.R
@@ -1,22 +1,44 @@
 read_geekswhodrink_release(tag = 'venue-quiz-results', name = '2501863925') -> df
-df |> 
+res <- df |> 
   select(-venue_id, -updated_at) %>% 
   split(.$quiz_date) |> 
   map(
     \(quiz_date_results) {
-      imap(
-        set_names(quiz_date_results$placing),
-        \(placing, .y) {
+      map(
+        quiz_date_results$placing,
+        \(placing) {
           team_quiz_results <- quiz_date_results[quiz_date_results$placing == placing, ]
           list(
+            'placing' = team_quiz_results$placing,
             'team' = team_quiz_results$team,
             'score' = team_quiz_results$score
           )
         }
       )
     }
-  ) |> 
-  jsonlite::toJSON(
+  )
+
+res <- df |> 
+  select(-venue_id, -updated_at) %>% 
+  split(.$quiz_date) |> 
+  map(
+    \(quiz_date_results) {
+    }
+  )
+res |> 
+  jsonlite::write_json(
+    'temp.json',
     pretty = TRUE,
     auto_unbox = TRUE
   )
+raw <- jsonlite::read_json('temp.json')
+imap_dfr(
+  raw,
+  \(quiz_date_results, .y) {
+    bind_cols(
+      tibble(quiz_date = lubridate::ymd(.y)),
+      bind_rows(quiz_date_results)
+    )
+  }
+)
+bind_rows(raw, .id = 'quiz_date')
