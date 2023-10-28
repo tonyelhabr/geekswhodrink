fce3a49dfe47eafb0fcab2231499ea7824208f60
Tony ElHabr <anthonyelhabr@gmail.com>
2023-10-28 07:35:08 -0500

    add austin venue scrape

--- src/0-scrape-geekswhodrink-venues.py
+++ src/0-scrape-geekswhodrink-venues.py
@@ -4,48 +4,55 @@ from bs4 import BeautifulSoup
 from selenium import webdriver
 import chromedriver_autoinstaller
 from datetime import datetime
+from urllib.parse import urlencode, urlunparse
 from utils import create_or_update_geekswhodrink_release
 
 chromedriver_autoinstaller.install()
-chrome_options = webdriver.ChromeOptions()    
+chrome_options = webdriver.ChromeOptions()
 options = [
-  '--headless',
+    '--headless',
 ]
 
 for option in options:
-  chrome_options.add_argument(option)
+    chrome_options.add_argument(option)
 
-driver = webdriver.Chrome(options = chrome_options)
+def build_geekswhodrink_venue_search_url(location: str = '') -> str:
+    query_params = {'search': '', 'location': location}
+    encoded_params = urlencode(query_params)
+    url = urlunparse(('https', 'www.geekswhodrink.com', 'venues', '', encoded_params, ''))
+    return url
 
-def scrape_geekswhodrink_venues_given_location(driver):
-  url = 'https://www.geekswhodrink.com/venues/'
-  driver.get(url)
-  time.sleep(3)
-  soup = BeautifulSoup(driver.page_source, 'html.parser')
-  results = soup.find_all('div', class_='find__col find__col--list')
-  
-  data = []
-  for result in results:
-    quiz_blocks = result.find_all('a', class_='find__block quizBlock quizBlock-returned')
-    for quiz_block in quiz_blocks:
-      data.append(
-        {
-          'venue_id': quiz_block['data-podio'],
-          'url': quiz_block['href'], 
-          'lat': quiz_block['data-lat'], 
-          'lon': quiz_block['data-lon'], 
-          'name': quiz_block['data-title'],
-          'address': quiz_block['data-address']
-        }
-      )
-      
-  return(pd.DataFrame(data))
+def scrape_geekswhodrink_venues_given_location(driver, location: str = '') -> pd.DataFrame:
+    url = build_geekswhodrink_venue_search_url(location=location)
+    driver.get(url)
+    time.sleep(3)
+    soup = BeautifulSoup(driver.page_source, 'html.parser')
+    results = soup.find_all('div', class_='find__col find__col--list')
 
-venues = scrape_geekswhodrink_venues_given_location(driver)
-current_time = datetime.now()
-venues['updated_at'] = current_time.strftime('%Y-%m-%d %H:%M:%S')
+    data = []
+    for result in results:
+        quiz_blocks = result.find_all('a', class_='find__block quizBlock quizBlock-returned')
+        for quiz_block in quiz_blocks:
+            data.append(
+                {
+                    'venue_id': quiz_block['data-podio'],
+                    'url': quiz_block['href'],
+                    'lat': quiz_block['data-lat'],
+                    'lon': quiz_block['data-lon'],
+                    'name': quiz_block['data-title'],
+                    'address': quiz_block['data-address']
+                }
+            )
 
-create_or_update_geekswhodrink_release(
-  df=venues,
-  file_name='venues.csv'
-)
+    return pd.DataFrame(data)
+
+def process_and_save_geekswhodrink_venues(location: str, file_name: str):
+    driver = webdriver.Chrome(options=chrome_options)
+    venues = scrape_geekswhodrink_venues_given_location(driver, location)
+    current_time = datetime.now()
+    venues['updated_at'] = current_time.strftime('%Y-%m-%d %H:%M:%S')
+    create_or_update_geekswhodrink_release(df=venues, file_name=file_name)
+    driver.quit()
+
+process_and_save_geekswhodrink_venues(location='', file_name='venues.csv')
+process_and_save_geekswhodrink_venues(location='Austin', file_name='austin-venues.csv')
