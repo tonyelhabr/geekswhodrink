1ae1184940e3f1bce74723f1b0d48d7c15108114
Tony ElHabr <anthonyelhabr@gmail.com>
2024-01-06 05:48:07 -0600

    try pb_upload_file

--- scripts/test.R
+++ scripts/test.R
@@ -1,7 +1,30 @@
 source(file.path('scripts', 'helpers.R'))
 venue_id <- 2180352964
-res <- read_venue_quiz_results(venue_id)
-write_quiz_results(
-  res,
-  venue_id
+x <- read_venue_quiz_results(venue_id)
+# write_quiz_results(
+#   res,
+#   venue_id
+# )
+res <- convert_quiz_results_df_to_list(x)
+
+tag <- 'venue-quiz-results'
+name <- as.character(venue_id)
+ext <- 'json'
+f <- function(x, path) { 
+  jsonlite::write_json(
+    x, 
+    path, 
+    auto_unbox = TRUE,
+    pretty = TRUE
+  ) 
+}
+temp_dir <- tempdir(check = TRUE)
+basename <- sprintf('%s.%s', name, ext)
+temp_path <- file.path(temp_dir, basename)
+f(x, temp_path)
+piggyback:::pb_upload_file(
+  temp_path,
+  .token = GITHUB_PAT,
+  repo = REPO,
+  tag = tag
 )
