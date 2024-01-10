da05f29d8edcbc98a97ac13c27006edfb33894c9
Tony ElHabr <anthonyelhabr@gmail.com>
2024-01-10 17:38:36 -0600

    try to compare what the browser sees before and after removing the pop up block

--- scripts/0-scrape-venues.py
+++ scripts/0-scrape-venues.py
@@ -10,6 +10,7 @@ import chromedriver_autoinstaller
 from datetime import datetime
 from urllib.parse import urlencode, urlunparse
 from dotenv import load_dotenv
+import time
 
 from src.utils import create_or_update_geekswhodrink_release
 
@@ -18,85 +19,71 @@ load_dotenv()
 # %%
 chromedriver_autoinstaller.install()
 chrome_options = webdriver.ChromeOptions()
-options = [
-    "--headless",
-]
+options = []
 
 for option in options:
     chrome_options.add_argument(option)
 
+#%%
+query_params = {"search": "", "location": ""}
+encoded_params = urlencode(query_params)
+url = urlunparse(
+    ("https", "www.geekswhodrink.com", "venues", "", encoded_params, "")
+)
 
-# %%
-def build_geekswhodrink_venue_search_url(location: str = "") -> str:
-    query_params = {"search": "", "location": location}
-    encoded_params = urlencode(query_params)
-    url = urlunparse(
-        ("https", "www.geekswhodrink.com", "venues", "", encoded_params, "")
-    )
-    return url
-
-
-def scrape_geekswhodrink_venues_given_location(
-    driver, location: str = ""
-) -> pd.DataFrame:
-    url = build_geekswhodrink_venue_search_url(location=location)
-    driver.get(url)
-    try:
-        # Look for the popup button by its class and type attributes
-        popup_button = WebDriverWait(driver, 10).until(
-            EC.presence_of_element_located(
-                (
-                    By.XPATH,
-                    '//button[@class="pum-close popmake-close" and @type="button"]',
-                )
+driver = webdriver.Chrome(options=chrome_options)
+driver.get(url)
+soup0 = BeautifulSoup(driver.page_source, "html.parser")
+print(str(soup0)[:100])
+try:
+    # Look for the popup button by its class and type attributes
+    popup_button = WebDriverWait(driver, 5).until(
+        EC.presence_of_element_located(
+            (
+                By.XPATH,
+                '//button[@class="pum-close popmake-close" and @type="button"]',
             )
         )
-        print('popup button found')
-        # Close the popup by clicking the button
-        popup_button.click()
-    except NoSuchElementException:
-        print("Popup button not found.")
-        
-    print("bar")
-    soup = BeautifulSoup(driver.page_source, "html.parser")
-    print(str(soup)[:200])
-    results = soup.find_all("div", class_="find__col find__col--list")
-    print(str(results[0])[:200])
-    data = []
-    for result in results:
-        quiz_blocks = result.find_all("a", class_="quizBlock-returned")
-        print(len(quiz_blocks))
-        for i, quiz_block in enumerate(quiz_blocks):
-            print(i)
-            data.append(
-                {
-                    "venue_id": quiz_block["data-podio"],
-                    "url": quiz_block["href"],
-                    "lat": quiz_block["data-lat"],
-                    "lon": quiz_block["data-lon"],
-                    "name": quiz_block["data-title"],
-                    "address": quiz_block["data-address"],
-                }
-            )
+    )
+    print('popup button found')
+    # Close the popup by clicking the button
+    popup_button.click()
+except NoSuchElementException:
+    print("Popup button not found.")
+    
+#%%
+time.sleep(5)
+soup = BeautifulSoup(driver.page_source, "html.parser")
+print(str(soup)[:200])
+#%%
+results = soup.find_all("div", class_="find__col find__col--list")
 
-    return pd.DataFrame(data)
+#%%
+data = []
+for result in results:
+    quiz_blocks = result.find_all("a", class_="quizBlock-returned")
+    print(len(quiz_blocks))
+    for i, quiz_block in enumerate(quiz_blocks):
+        data.append(
+            {
+                "venue_id": quiz_block["data-podio"],
+                "url": quiz_block["href"],
+                "lat": quiz_block["data-lat"],
+                "lon": quiz_block["data-lon"],
+                "name": quiz_block["data-title"],
+                "address": quiz_block["data-address"],
+            }
+        )
 
+venues = pd.DataFrame(data)
+driver.quit()
 
-def process_and_save_geekswhodrink_venues(
-    location: str, file_name: str
-) -> pd.DataFrame:
-    driver = webdriver.Chrome(options=chrome_options)
-    venues = scrape_geekswhodrink_venues_given_location(driver, location)
-    current_time = datetime.now()
-    print(venues.shape)
-    venues["updated_at"] = current_time.strftime("%Y-%m-%d %H:%M:%S")
-    create_or_update_geekswhodrink_release(df=venues, file_name=file_name)
-    driver.quit()
-    return venues
+current_time = datetime.now()
+venues["updated_at"] = current_time.strftime("%Y-%m-%d %H:%M:%S")
 
+create_or_update_geekswhodrink_release(df=venues, file_name="venues.csv")
 
 # %%
-venues = process_and_save_geekswhodrink_venues(location="", file_name="venues.csv")
 print(venues.shape)
 
 # %%
