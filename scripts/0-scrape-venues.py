366c9b5c87a14300482ca2b9e5d8251b187a871c
Tony ElHabr <anthonyelhabr@gmail.com>
2024-01-11 07:05:09 -0600

    clean up venue script

--- scripts/0-scrape-venues.py
+++ scripts/0-scrape-venues.py
@@ -11,12 +11,12 @@ from datetime import datetime
 from urllib.parse import urlencode, urlunparse
 from dotenv import load_dotenv
 import time
+import sys
 
 from src.utils import create_or_update_geekswhodrink_release
 
 load_dotenv()
 
-# %%
 chromedriver_autoinstaller.install()
 chrome_options = webdriver.ChromeOptions()
 options = ["--headless"]
@@ -24,20 +24,15 @@ options = ["--headless"]
 for option in options:
     chrome_options.add_argument(option)
 
-#%%
 query_params = {"search": "", "location": ""}
 encoded_params = urlencode(query_params)
-url = urlunparse(
-    ("https", "www.geekswhodrink.com", "venues", "", encoded_params, "")
-)
+url = urlunparse(("https", "www.geekswhodrink.com", "venues", "", encoded_params, ""))
 
 driver = webdriver.Chrome(options=chrome_options)
 driver.get(url)
-soup0 = BeautifulSoup(driver.page_source, "html.parser")
-print(str(soup0)[:100])
+
 try:
-    # Look for the popup button by its class and type attributes
-    popup_button = WebDriverWait(driver, 5).until(
+    popup_button = WebDriverWait(driver, 7).until(
         EC.presence_of_element_located(
             (
                 By.XPATH,
@@ -45,24 +40,20 @@ try:
             )
         )
     )
-    print('popup button found')
-    # Close the popup by clicking the button
     popup_button.click()
 except NoSuchElementException:
     print("Popup button not found.")
-    
-#%%
+    driver.quit()
+    print("Exiting the script early and not updating venues.csv.")
+    sys.exit()
+
 time.sleep(5)
 soup = BeautifulSoup(driver.page_source, "html.parser")
-print(str(soup)[:200])
-#%%
 results = soup.find_all("div", class_="find__col find__col--list")
 
-#%%
 data = []
 for result in results:
     quiz_blocks = result.find_all("a", class_="quizBlock-returned")
-    print(len(quiz_blocks))
     for i, quiz_block in enumerate(quiz_blocks):
         data.append(
             {
@@ -82,8 +73,3 @@ current_time = datetime.now()
 venues["updated_at"] = current_time.strftime("%Y-%m-%d %H:%M:%S")
 
 create_or_update_geekswhodrink_release(df=venues, file_name="venues.csv")
-
-# %%
-print(venues.shape)
-
-# %%
